11eb8b1697bf5ffec208cac8b98469eb4f88248c
Noah <npthomas@pugetsound.edu>
2019-11-25 13:56:21 +0100

    test commit2

--- app/src/main/java/hu/ait/androidfinal/MainActivity.kt
+++ app/src/main/java/hu/ait/androidfinal/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-    var test = "test"
+    var test = "testing"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
