5f6250820994cd79c36c5943407d4ef0199632b0
Noah <npthomas@pugetsound.edu>
2019-11-25 14:02:37 +0100

    test commit3

--- app/src/main/java/hu/ait/androidfinal/MainActivity.kt
+++ app/src/main/java/hu/ait/androidfinal/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-    var test = "testing"
+    var test = "this is a test"
 
     var test0 = "shithub"
     override fun onCreate(savedInstanceState: Bundle?) {
