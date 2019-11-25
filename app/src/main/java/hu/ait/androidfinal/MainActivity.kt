12ab7180fec52f513ffcbbb415e7cdbe4bfc8a37
Noah <npthomas@pugetsound.edu>
2019-11-25 14:16:40 +0100

    test commit3

--- app/src/main/java/hu/ait/androidfinal/MainActivity.kt
+++ app/src/main/java/hu/ait/androidfinal/MainActivity.kt
@@ -5,9 +5,6 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-    var test = "testing"
-
-    var test0 = "shithub"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
